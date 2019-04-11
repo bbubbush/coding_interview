51352b661a6994da6caa1f0970319e974a33c09b
bbubbush <bbubbush@gmail.com>
2019-04-11 23:44:23 +0900

    2019-04-11

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import _01_ArrayAndString.Duplecate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Duplecate d = new Duplecate();
+        System.out.println(d.solution("abc 123"));
+
+
+    }
+}
